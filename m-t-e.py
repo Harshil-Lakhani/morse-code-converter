404761d052da528b239885703b153b2253c3d8b7
Harshil-Lakhani <hlakhani2004@gmail.com>
2026-08-27 12:17:36 +0530

    Update m-t-e.py

--- m-t-e.py
+++ m-t-e.py
@@ -1,13 +1,39 @@
 import re
-mydic = {'A' : '.-', 'H' : '....','I':'..'}
+mydic = {  'A':'.-',     'B':'-...',
+                'C':'-.-.',   'D':'-..',     'E':'.',
+                'F':'..-.',   'G':'--.',     'H':'....',
+                'I':'..',     'J':'.---',    'K':'-.-',
+                'L':'.-..',   'M':'--',      'N':'-.',
+                'O':'---',    'P':'.--.',    'Q':'--.-',
+                'R':'.-.',    'S':'...',     'T':'-',
+                'U':'..-',    'V':'...-',    'W':'.--',
+                'X':'-..-',   'Y':'-.--',    'Z':'--..',
+                '1':'.----',  '2':'..---',   '3':'...--',
+                '4':'....-',  '5':'.....',   '6':'-....',
+                '7':'--...',  '8':'---..',   '9':'----.',
+                '0':'-----',  ', ':'--..--', '.':'.-.-.-',
+                '?':'..--..', '/':'-..-.',   '-':'-....-',
+                '(':'-.--.',  ')':'-.--.-' }
 
 temp=input("enter morse code: ")
-pattern = re.findall(".|-",temp)
-if pattern==True:
+pattern = re.match(".|-",temp)
+
+#for single character
+# if pattern:
+#     for key, value in mydic.items():
+#         if value == temp:
+#             print(key)   
+# else: 
+#     print("Please enter morse code")
+
+# for multiple charcters
+letters = temp.split(" ")
+
+result = ""
+for letter in letters:
     for key, value in mydic.items():
-        if value == temp:
-            print(key)   
-else: 
-    print("Please enter morse code")
+        if value == letter:
+            result += key
+print(result)
 
 
